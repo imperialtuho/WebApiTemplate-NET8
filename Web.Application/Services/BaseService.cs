b9eda9d2fceb3dd7f5e35a8786fafa9ee3849456
Tu.Ho <tu.ho@mercatus.com>
2024-12-17 17:19:40 +0700

    feature/development | adds `IsCurrentPerformingOperationValid` function.

--- Web.Application/Services/BaseService.cs
+++ Web.Application/Services/BaseService.cs
@@ -30,6 +30,11 @@ namespace Web.Application.Services
             }
         }
 
+        /// <summary>
+        /// Checking the action is performed by admin or not.
+        /// </summary>
+        /// <param name="currentUser">The currentUser.</param>
+        /// <returns>True/False based on currentUser.</returns>
         protected static bool IsActionPerformByAdmin(UserSession? currentUser = null)
         {
             if (currentUser is null)
@@ -38,6 +43,36 @@ namespace Web.Application.Services
             }
 
             if (currentUser.Roles is not null && currentUser.Roles.Exists(r => r.Contains(ApplicationDefaultRoleValue.SuperAdmin) || r.Contains(ApplicationDefaultRoleValue.Admin)))
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        /// Checks the action is being performed by admin or its owner.
+        /// </summary>
+        /// <param name="ownerId">The data's ownerId.</param>
+        /// <returns>True if action is performed by admin or the ownerId matched with its own data.</returns>
+        protected bool IsCurrentPerformingOperationValid(string? ownerId = null)
+        {
+            UserSession? loginSession = LoginSession;
+
+            // if the action performs by admins -> valid
+            if (loginSession is not null && IsActionPerformByAdmin(loginSession))
+            {
+                return true;
+            }
+
+            // ownerId is null -> invalid
+            if (ownerId == null)
+            {
+                return false;
+            }
+
+            // Checks owner's data to its action. If owner's data matched with the provided ownerId -> valid
+            if (!string.IsNullOrEmpty(ownerId) && ownerId.Equals(loginSession?.UserId))
             {
                 return true;
             }
